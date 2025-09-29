6053f1f66bc58f045923d7c3549ae5d757413f58
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-09-29 09:20:43 -0700

    Prompting for name

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Jaren 9/29
+# ask user for a name
+name = input("What is your name?")
+
+#print hello to the user
+print( 'Hello ' + name )
