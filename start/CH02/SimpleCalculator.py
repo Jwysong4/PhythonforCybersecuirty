c18d7924769f509814c58b6924eb9a5ea8aafebd
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-01 09:40:56 -0700

    math calc

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -3,12 +3,27 @@
 # Created 10/01 by jaren
 # Get two numbers from user
 first_num = float( input("First number = ") )
+operation = input("What is the operation (+,-,*,/,**,//,%) ")
 second_num = float(input("Second number = ") )
 
 
 # Add numbers and print answer
-print(f"{first_num} + {second_num} = ", first_num + second_num)
-print(f"{first_num} - {second_num} = ", first_num - second_num)
-print(f"{first_num} * {second_num} = ", first_num * second_num)
-print(f"{first_num} / {second_num} = ", first_num / second_num)
-
+if operation == "+": 
+    print(f"{first_num} + {second_num} = ", 
+    first_num + second_num)
+if operation == "-":
+     print(f"{first_num} - {second_num} = ", 
+     first_num - second_num)
+if operation == "*":
+     print(f"{first_num} * {second_num} = ", 
+    first_num * second_num)
+    if operation == "**":
+    print(f"{first_num} ** {second_num} = ", 
+    first_num ** second_num)
+ if operation == "//":
+    print(f"{first_num} // {second_num} = ", 
+    first_num // second_num)
+ if operation == "%":
+     print(f"{first_num} % {second_num} = ", 
+    first_num % second_num)
+print("Done")
