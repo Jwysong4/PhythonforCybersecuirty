fd4bdf2727c4d8cabe85fdf2562ad3c30116d39d
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-06 12:49:16 -0700

    started confidential

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+
+#input ("Is today a good day Y/N?")
+#if ("Is today a good day? Y/N?") == "Y":
+    #print ("Yes it is")
+
+
+
+
+
+
+
+
+for x in range(10):
+    print("Yeah it is")
--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -2,14 +2,16 @@
 # example workign with conditionals
 #By 
 
-score = int(input("What was your score? "))
+score = int(input(" What is your score? "))
 if score > 90: score = 75
-    print("You got a A!")
+print("You got a A!")
 elif score > 80:
-    print("You got a B")
+print("You got a B")
 elif score > 70:
-    print("You got a C")
+print("You got a C")
 elif score > 60:
-    print("You got a D")
+print("You got a D")
 else:
-    print("you got an F")
+print("you got an F")
+for x in range(10):
+    print("Yeah it is")
