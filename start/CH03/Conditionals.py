b5fc027b02f0eca89264cda7e404136143177d57
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-06 09:50:59 -0700

    started ping

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+
+score = int(input("What was your score? "))
+if score > 90: score = 75
+    print("You got a A!")
+elif score > 80:
+    print("You got a B")
+elif score > 70:
+    print("You got a C")
+elif score > 60:
+    print("You got a D")
+else:
+    print("you got an F")
--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By Jaren 10/06/25
+
+# Import things
+import os
+
+# Assign IP to variable
+Ip_addr = "127."
+
+# Build ping command
+ping_cmd = "ping -c 1 -W 2 " + Ip_addr + " > /dev/null 2>&1"
+print(ping_cmd) 
+# Execute ping command
+exit_code = os.system( ping_cmd )
+# Print results
+print( exit_code )
+if exit_code == 0:
+    print(f"{Ip_addr} is online")
+else:
+    print(f"{Ip_addr} is NOT online")
