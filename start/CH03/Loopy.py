81deb78a612eecc1153515d53be64421350f9142
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-08 08:51:17 -0700

    loops

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+
+def hi():
+    for i in range(10):
+        print("Hello World")
+        print(i)
+print("Starting")
+hi()
+print("done")
+
+
