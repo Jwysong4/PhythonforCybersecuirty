b9c1961b837fd259b930a73569e76347a060bfef
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-08 09:29:57 -0700

    pinging

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -7,31 +7,32 @@ import os
 import platform
 
 # Assign IP to variable
-Ip_addr = "127.0.0.1"
+Ip_prefix = "192.168.0."
+
+#start loop
+for final_octet in range(254):
+    ip_addr = Ip_prefix + str(final_octet + 1)
 
 # Find current OS
-current_os = platform.system().lower()
+    current_os = platform.system().lower()
 
 # If windows
 if current_os == "windows":
-    ping_cmd = "ping -n 1 -w 2 " + Ip_addr + " > nul"
+    ping_cmd = "ping -n 1 -w 2 " + ip_addr + " > nul"
 else: 
-    ping_cmd = "ping -c 1 -W 2 " + Ip_addr + " > /dev/null 2>&1"
-
-#print(ping_cmd)
-
+    ping_cmd = "ping -c 1 -W 2 " + ip_addr + " > /dev/null 2>&1"
 
 # Build ping command
-ping_cmd = "ping -c 1 -W 2 " + Ip_addr + " > /dev/null 2>&1"
+ping_cmd = "ping -c 1 -W 2 " + ip_addr + " > /dev/null 2>&1"
 print(ping_cmd) 
 
 # Execute ping command
-exit_code = os.system( ping_cmd )
+# exit_code = os.system( ping_cmd )
 # Print results
 print( exit_code )
 if exit_code == 0:
-    print(f"{Ip_addr} is online")
+    print(f"{ip_addr} is online")
 else:
-    print(f"{Ip_addr} is NOT online")
+    print(f"{ip_addr} is NOT online")
 
 #
