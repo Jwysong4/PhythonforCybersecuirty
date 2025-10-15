4a224d0e5f30f920bc9c3570e978ee942d65b6be
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-15 09:04:48 -0700

    ping

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -6,34 +6,36 @@
 import os
 import platform
 
+def ping_address(ip_address):
+    # Find current OS
+    current_os = platform.system().lower()
+# Build ping command
+# If windows
+if current_os == "windows":
+    ping_cmd = "ping -n 1 -w 2 " + ip_address + " > nul"
+else: 
+    ping_cmd = "ping -c 1 -W 2 " + ip_address + " > /dev/null 2>&1"
+
+ping_cmd = "ping -c 1 -W 2 " + ip_address + " > /dev/null 2>&1"
+print(ping_cmd) 
+exit_code = os.system( ping_cmd )
+return exit_code
+
+# Execute ping command
+
+
 # Assign IP to variable
 Ip_prefix = "192.168.0."
 
 #start loop
 for final_octet in range(254):
-    # Build IP address
-    ip_addr = Ip_prefix + str(final_octet + 1)
+    ip_addr = ip_prefix = str(final_octet =1)
 
-# Find current OS
-    current_os = platform.system().lower()
+    #call function
+    exit_code = ping_addresses(ip_addr)
 
-# If windows
-if current_os == "windows":
-    ping_cmd = "ping -n 1 -w 2 " + ip_addr + " > nul"
-else: 
-    ping_cmd = "ping -c 1 -W 2 " + ip_addr + " > /dev/null 2>&1"
-
-# Build ping command
-ping_cmd = "ping -c 1 -W 2 " + ip_addr + " > /dev/null 2>&1"
-print(ping_cmd) 
-
-# Execute ping command
-# exit_code = os.system( ping_cmd )
 # Print results
-print( exit_code )
 if exit_code == 0:
     print(f"{ip_addr} is online")
 else:
     print(f"{ip_addr} is NOT online")
-
-#
