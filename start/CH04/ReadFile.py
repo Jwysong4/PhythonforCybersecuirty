91e283857445b1a2a7cf48ab4c41c3e1345dad99
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-10-15 09:37:03 -0700

    Sample scripts

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
 # By 
+
+#!/usr/bin/env python3
+# Sample script that writes to a file
+# By Jaren 10/15
+
+import os
+#get current directory
+script_path = os.path.abspath( __file__ )
+script_dir = os.path.dirname( script_path )
+#Build file path
+file_path = os.path.join(script_dir , "testfile.txt")
+
+
+
+# Create file object
+f = open("file_path", "r")
+
+#read file object
+print( f.read())
+
+# Close file object
+f.close()
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Jaren 10/15
+
+import os
+#get current directory
+script_path = os.path.abspath( __file__ )
+script_dir = os.path.dirname( script_path )
+#Build file path
+file_path = os.path.join(script_dir , "testfile.txt")
+
+
+
+# Create file object
+f = open("file_path", "w")
+
+# Write to file object
+f.write("My name is Jaren\n")
+f.write("I like football\n")
+f.write("Hello World\n")
+
+# Close file object
+f.close()
