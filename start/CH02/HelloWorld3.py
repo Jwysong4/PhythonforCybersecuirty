afbb6805b3c61a49767894d268d614512cd5c2f8
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-11-19 10:56:52 -0800

    homework 11/19/25

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -2,5 +2,19 @@
 # A more complex "Hello World" script in python with Inputs
 # Created 
 
+import random
+import string
 
+def generate_password(length=12, use_digits=True, use_symbols=True, use_uppercase=True):
+    chars = list(string.ascii_lowercase)
+    if use_uppercase:
+        chars += list(string.ascii_lowercase)
+    if use_symbols:
+        chars += list("!@#$%^&*()-_=+[]{};:,.<>?/")
+    if not chars:
+        raise ValueError("No character types selected!")
+    password = ''.join(random.choice(chars) for _ in range (length))
+    return password
 
+if __name__ == "__main__":
+    print("Generated password:", generate_password(length=16))
