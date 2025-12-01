cbe98d55f24cf3ccaca6a965136acc0d21991094
Jwysong4 <Wysong.Jaren@students.greenriver.edu>
2025-12-01 11:46:02 -0800

    gui application HW

--- start/CH09/CheckURL.py
+++ start/CH09/CheckURL.py
@@ -2,3 +2,25 @@
 # Script that checks URLs against Google's Safe Browsing API
 # https://developers.google.com/safe-browsing/v4
 # By 
+
+import tkinter as tk
+window =tk.Tk()
+
+label_prompt =tk.Label(window, text="Enter total amount:")
+label_prompt.pack(pady=5)
+
+entry_amount =tk.Entry(window)
+entry_amount.pack(pady=5)
+
+label_result =tk.Label(window, text="")
+label_result.pack(pady=10)
+
+def calculate_tip():
+    amount = entry_amount.get()
+    tip = float(amount) * 0.20
+    label_result.config(text=f"Tip: ${tip:.2}")
+
+button_calc = tk.Button(window, text="Calculate 20% Tip", command=calculate_tip)
+button_calc.pack(pady=10)
+
+window.mainloop()
